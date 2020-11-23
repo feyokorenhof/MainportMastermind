3ddf0a8a49ef7c483c93feeb97d1e6f6738f64ce
thrytas <58788402+thrytas@users.noreply.github.com>
2020-11-23 13:49:37 +0100

    Add files via upload
    dark mode

--- MainportMastermind/Dobbel.py
+++ MainportMastermind/Dobbel.py
@@ -17,9 +17,6 @@ class Dobbel():
         self.rolling = True
         
     def drawDice(self, w):
-        fill(255, 255, 255)
-        rectMode(CENTER)
-        rect((width/4)*3, 600, 500, 500)
         imageMode(CENTER)
         img = self.images[self.rolledDice-1]
         image(img, (width/4)*w, 600, 490, 490)
@@ -30,9 +27,6 @@ class Dobbel():
             self.rolling = False
             return
                 
-        fill(255, 255, 255)
-        rectMode(CENTER)
-        rect((width/4)*3, 600, 500, 500)
         imageMode(CENTER)
         img = self.images[self.animationFrame]
         image(img, (width/4)*w, 600, 490, 490)
@@ -42,7 +36,7 @@ class Dobbel():
     
     
 def upperText():
-    fill(0, 0, 0) #text color (black)
+    fill(255, 255, 255) #text color (black)
     textSize(45)
     textAlign(CENTER)
     text('Gooi de linker of rechter dobbelsteen', width/2, 175)
@@ -50,10 +44,12 @@ def upperText():
     text('Dobbel en bepaal wat je deze beurt mag doen', width/2, 275)
 
 def rollButtons(): #the lower two buttons
-    fill(255, 255, 255)
-    rectMode(CENTER)
-    rect(width/4, 1000, 350, 200)
     fill(0, 0, 0)
+    rectMode(CENTER)
+    rect(width/4, 950, 355, 155)
+    fill(61, 61, 62)
+    rect(width/4, 950, 350, 150)
+    fill(255, 255, 255)
     textSize(40)
     textAlign(CENTER)
-    text('Gooi!', width/4, 1010)       
+    text('Gooi!', width/4, 950)       
